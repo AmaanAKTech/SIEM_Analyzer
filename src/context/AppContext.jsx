bc98f01451f6f07ffb96e6973bf13b6261c96aa5
Mir Amaan Ali Khan <amaanak.tech@gmail.com>
2025-06-22 08:02:24 +0530

    Update AppContext.jsx

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -7,15 +7,18 @@ export const AppProvider = ({ children }) => {
   const [logs, setLogs] = useState([]);
   const [filteredLogs, setFilteredLogs] = useState([]);
   const [query, setQuery] = useState('');
+  const [alerts, setAlerts] = useState([]);
 
-  const value = {
-    logs,
-    setLogs,
-    filteredLogs,
-    setFilteredLogs,
-    query,
-    setQuery,
-  };
+const value = {
+  logs,
+  setLogs,
+  filteredLogs,
+  setFilteredLogs,
+  query,
+  setQuery,
+  alerts,
+  setAlerts,
+};
 
   return (
     <AppContext.Provider value={value}>
