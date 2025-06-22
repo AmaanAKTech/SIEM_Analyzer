0d44368f5361d46c94cc038dd4b4ea6af0b24746
Mir Amaan Ali Khan <amaanak.tech@gmail.com>
2025-06-22 08:21:46 +0530

    Update AppContext.jsx

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,24 +1,20 @@
-// src/context/AppContext.jsx
 import React, { createContext, useState } from 'react';
 
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const [logs, setLogs] = useState([]);
-  const [filteredLogs, setFilteredLogs] = useState([]);
+  const [filteredLogs, setFilteredLogs] = useState([]); // important
   const [query, setQuery] = useState('');
-  const [alerts, setAlerts] = useState([]);
 
-const value = {
-  logs,
-  setLogs,
-  filteredLogs,
-  setFilteredLogs,
-  query,
-  setQuery,
-  alerts,
-  setAlerts,
-};
+  const value = {
+    logs,
+    setLogs,
+    filteredLogs,
+    setFilteredLogs,
+    query,
+    setQuery,
+  };
 
   return (
     <AppContext.Provider value={value}>
